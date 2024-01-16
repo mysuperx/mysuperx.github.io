3c5e2f4494707dbe133bbd1df0e271ce0b4b1d1f
mysuperx <156839142+mysuperx@users.noreply.github.com>
2024-01-17 02:07:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-xiexie
+<input type="text" name="addend10" size="4" style="width:66px;height:36px;" value="6.4">
+	 
+	 &nbsp;&nbsp; <a href="https://www.google.com/search?q=1+usd+to+cny" target="_blank">汇率</a>
+	 &nbsp;&nbsp; <a href="./yupoourl/">导航</a>
+	 &nbsp;&nbsp; <a href="./epacket1.php">E邮宝</a>
+	 &nbsp;&nbsp; <a href="./postnl/xinganpostnl.php">兴安Postnl</a>
+	 &nbsp;&nbsp; <a href="./wenben/account.php" target="_blank">Account</a>
+	 &nbsp;&nbsp; <a href="./wenben/catalog.php" target="_blank">Catalog</a>
+	 &nbsp;&nbsp; <a href="./shipping/sizenew.jpg" target="_blank">Sizenew</a>
+	 &nbsp;&nbsp; <a href="./wenben/size.php" target="_blank">Size</a>
+	 &nbsp;&nbsp; <a href="./shipping/tw.jpg" target="_blank">Wise</a>
+	 &nbsp;&nbsp; <a href="./wenben/Revolut.php" target="_blank">Revolut</a>
+	 &nbsp;&nbsp; <a href="./shipping/wu.jpg" target="_blank">WU</a>
+	 &nbsp;&nbsp; <a href="./shipping/266954797.jpg" target="_blank">USDT</a>
+	 &nbsp;&nbsp; <a href="./shipping/TBAsUFRXdZwM3seLDNqDyyLhPzbH7Pa6MN.jpg" target="_blank">TRC20</a>
+	 &nbsp;&nbsp; <a href="./shipping/gotmoney.jpg" target="_blank">收款码</a>
+	 &nbsp;&nbsp; <a href="https://uutool.cn/english-name/" target="_blank">名字</a>
+	 &nbsp;&nbsp; <a href="./it.php" target="_blank">IT</a>
+	 &nbsp;&nbsp; <a href="./brcity.php" target="_blank">BRcity</a>
+	 &nbsp;&nbsp; <a href="./duibi/" target="_blank">对比</a>
+	 &nbsp;&nbsp; DBX01165546	 
+	 
+	 <br><br>
